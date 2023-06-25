1d80bc0f4eaf5da3bb39b5068d2e4e5460929c16
artemo8913 <kaline-son@ya.ru>
2023-06-25 09:19:05 +0700

    fix layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,171 @@ import { TicketCard } from "@/widgets/ticketCard";
 export default function Home() {
   return (
     <main>
-      MAIN PAGE
+      MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum
+      assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique
+      labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore
+      eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum,
+      voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis
+      repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur
+      adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum
+      sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit,
+      amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus
+      nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto
+      labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam
+      sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum
+      assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique
+      labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore
+      eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum,
+      voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis
+      repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur
+      adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum
+      sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit,
+      amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus
+      nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto
+      labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam
+      sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum
+      assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique
+      labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore
+      eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum,
+      voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis
+      repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur
+      adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum
+      sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit,
+      amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus
+      nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto
+      labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam
+      sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum
+      assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique
+      labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore
+      eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum,
+      voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis
+      repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur
+      adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum
+      sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit,
+      amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus
+      nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto
+      labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam
+      sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum
+      assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique
+      labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore
+      eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum,
+      voluptatem similique labore rerum ullam sit. voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum
+      dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore
+      doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit.
+      MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum
+      assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique
+      labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore
+      eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum,
+      voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis
+      repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur
+      adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum
+      sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit,
+      amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus
+      nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. voluptatem
+      similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
+      tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat
+      dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur
+      adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum
+      sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit,
+      amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus
+      nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto
+      labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam
+      sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum
+      assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique
+      labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore
+      eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum,
+      voluptatem similique labore rerum ullam sit. voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum
+      dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore
+      doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit.
+      MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum
+      assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique
+      labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore
+      eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum,
+      voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis
+      repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur
+      adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum
+      sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit,
+      amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus
+      nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. voluptatem
+      similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
+      tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat
+      dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur
+      adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum
+      sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit,
+      amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus
+      nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto
+      labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam
+      sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum
+      assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique
+      labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore
+      eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum,
+      voluptatem similique labore rerum ullam sit. voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum
+      dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore
+      doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit.
+      MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum
+      assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique
+      labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore
+      eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum,
+      voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis
+      repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur
+      adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum
+      sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit,
+      amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus
+      nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. voluptatem
+      similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
+      tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat
+      dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur
+      adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum
+      sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit,
+      amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus
+      nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto
+      labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam
+      sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum
+      assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique
+      labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore
+      eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum,
+      voluptatem similique labore rerum ullam sit. voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum
+      dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore
+      doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit.
+      MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum
+      assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique
+      labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore
+      eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum,
+      voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis
+      repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur
+      adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum
+      sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit,
+      amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus
+      nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. voluptatem
+      similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
+      tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat
+      dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur
+      adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum
+      sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit,
+      amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto labore doloribus
+      nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam sit. MAIN PAGE
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum assumenda iusto
+      labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique labore rerum ullam
+      sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore eveniet, mollitia eum
+      assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum, voluptatem similique
+      labore rerum ullam sit. MAIN PAGE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore tempore
+      eveniet, mollitia eum assumenda iusto labore doloribus nostrum delectus. Harum sunt veritatis repellat dolorum,
+      voluptatem similique labore rerum ullam sit.
       <TicketCard genre="genre" pictureUrl="../shared/assets/icons/basket.svg" title="title" value={0} />
       <QuestionCard answer="asasdsad" question="a123asfd" />
     </main>
